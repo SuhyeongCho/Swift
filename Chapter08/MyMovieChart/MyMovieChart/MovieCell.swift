897ea4aaf3f5ca00150fa1be35f6b605fee14ebd
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2019-05-12 23:01:37 +0900

    테이블 뷰

---
+++ Chapter08/MyMovieChart/MyMovieChart/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by 조수형 on 12/05/2019.
+//  Copyright © 2019 조수형. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet var title: UILabel!
+    @IBOutlet var desc: UILabel!
+    @IBOutlet var opendate: UILabel!
+    @IBOutlet var rating: UILabel!
+    
+    @IBOutlet var thumbnail: UIImageView!
+    
+}
---
+++ Chapter08/MyMovieChart/MyMovieChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by 조수형 on 12/05/2019.
+//  Copyright © 2019 조수형. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
